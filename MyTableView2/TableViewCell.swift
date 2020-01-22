78eec62693b8ada61a2cf5976d9beb78df669fe1
Dhanush <dhanush@dhanush.corp.capgemini.com>
2020-01-22 17:10:18 +0530

    Last Commit

---
+++ MyTableView2/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  MyTableView2
+//
+//  Created by Deepika R on 27/02/19.
+//  Copyright © 2019 Deepika R. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
